fa18808cc9af36841e39e0b7da2cc1b06d1280e6
Jakob Rolik <rolik.jakob@gmail.com>
2019-04-16 20:33:58 +0200

    Tag List

---
+++ src/components/AllQuotes/TagItem.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="tag">
+        <span>{{name}}</span>
+        <div class="spacer"></div>
+        <div class="stats">
+            <v-icon dark>mdi-format-quote-close</v-icon><span>{{numOfQuotes}}</span>
+            <v-icon dark class="heart">mdi-heart</v-icon><span>{{numOfLikes}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        numOfQuotes: {
+            type: Number,
+            required: true,
+        },
+        numOfLikes: {
+            type: Number,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.tag {
+    border: 1px solid white;
+    border-radius: 5px;
+    padding: 10px;
+    margin-top: 1rem;
+    display: flex;
+    align-items: center;
+}
+.spacer {
+    display: flex;
+    flex: 1 1 auto;
+    
+}
+.stats {
+    .v-icon {
+        margin-left: 10px;
+    }
+    span {
+        font-size: 1.2rem;
+        margin-left: 5px;
+    }
+}
+.heart {
+    font-size: 1.5rem;
+}
+</style>
--- src/views/AllQuotes.vue
+++ src/views/AllQuotes.vue
@@ -1,17 +1,37 @@
 <template>
     <v-container>
         <h1>Tags</h1>
-        
+        <v-text-field v-model="searchTerm" dark color="white" :append-icon="'search'"></v-text-field>
+        <TagListItem
+            v-for="tag in tagsToDisplay" :key="tag.name"
+            :numOfLikes="tag.numOfPositiveLikes"
+            :numOfQuotes="tag.numOfQuotes"
+            :name="tag.value"
+        />
     </v-container>
 </template>
 <script>
 import store from "@/store";
+import TagListItem from '@/components/AllQuotes/TagItem'
+
 export default {
+    components: {TagListItem},
+    data() {
+        return {
+            searchTerm: ""
+        }
+    },
     beforeRouteEnter(to, from, next) {
         store.dispatch('fetchAllTags').then(() => {
             console.log('action resolved');
             next()
         })
+    },
+    computed: {
+        tagsToDisplay() {
+            return this.tags.filter(tag => this.searchTerm === "" || tag.value.indexOf(this.searchTerm) > -1)
+        },
+        tags() {return this.$store.getters.getAllTags}
     }
 };
 </script>
